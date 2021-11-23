a331ab3b9442da1618f8785ea2a3ba41e7c8410e
TylerDurden75 <raytibo.j@gmail.com>
2021-11-23 18:23:16 +0100

    Security pass recreate

--- client/src/pages/user/Password.js
+++ client/src/pages/user/Password.js
@@ -1,13 +1,68 @@
-import React from "react";
-// import UserNav from "../../components/nav/UserNav";
+import React, { useState } from "react";
+import { getAuth, updatePassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
-const Password = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col">user Password page</div>
+const Password = () => {
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    await updatePassword(user, password)
+      .then(() => {
+        setLoading(false);
+        setPassword("");
+        toast.success("Password updated");
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast.error(err.message);
+      });
+  };
+
+  const passwordUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Your Password</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          className="form-control"
+          placeholder="Enter new password"
+          disabled={loading}
+          minLength="6"
+        />
+        <button
+          className="btn btn-primary"
+          disabled={!password || password.length < 6 || loading}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Password Update</h4>
+          )}
+          {passwordUpdateForm()}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Password;

3753dff1977a2180b39aeccf51e93917e73b4792
TylerDurden75 <raytibo.j@gmail.com>
2021-11-24 16:37:58 +0100

    Leaving unusefull Outlet

--- client/src/components/nav/UserNav.js
+++ client/src/components/nav/UserNav.js
@@ -1,30 +1,27 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const UserNav = () => {
   return (
-    <>
-      <nav>
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <Link to="/user/history" className="nav-link">
-              History
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/user/password" className="nav-link">
-              Password
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/user/wishlist" className="nav-link">
-              Whislist
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </>
+    <nav>
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <Link to="/user/history" className="nav-link">
+            History
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/user/password" className="nav-link">
+            Password
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/user/wishlist" className="nav-link">
+            Whislist
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 

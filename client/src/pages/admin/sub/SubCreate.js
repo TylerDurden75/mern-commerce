75970c494ba5589dfff27926744352b5040f7487
TylerDurden75 <raytibo.j@gmail.com>
2021-11-24 14:10:23 +0100

    Create Subcat

--- client/src/pages/admin/sub/SubCreate.js
+++ client/src/pages/admin/sub/SubCreate.js
@@ -1,14 +1,118 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+import { createSub, getSubs, removeSub } from "../../../functions/sub";
+import { getCategories } from "../../../functions/category";
+
 import AdminNav from "../../../components/nav/AdminNav";
+import CategoryForm from "../../../components/forms/CategoryForm";
+import LocalSearch from "../../../components/forms/LocalSearch";
 
 const SubCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
+  // searching filter
+  /* step 1 */
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    showCategories();
+  }, []);
+
+  const showCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createSub({ name, parent: category }, user.token)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created`);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        if (error.response.status === 400) toast.error(error.response.data);
+      });
+  };
+
+  const handleRemove = async (slug) => {
+    if (window.confirm("Delete ?")) {
+      setLoading(true);
+      removeSub(slug, user.token)
+        .then((res) => {
+          setLoading(false);
+          toast.success(`${res.data.name} deleted`);
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            setLoading(false);
+            toast.error(err.response.data);
+          }
+        });
+    }
+  };
+
+  /* step 3 */
+
+  // const handleSearchChange = (e) => {
+  //   e.preventDefault();
+  //   setKeyword(e.target.value.toLowerCase());
+  // };
+
+  /* step 4 */
+  const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword);
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col-md-10">sub create page</div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Create sub category</h4>
+          )}
+
+          <div className="form-group">
+            <label>Parent Category</label>
+            <select
+              name="category"
+              className="form-control"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option>Please select</option>
+              {categories.length > 0 &&
+                categories.map((c) => (
+                  <option key={c._id} value={c._id}>
+                    {c.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+          {/* step 2 */}
+          <LocalSearch keyword={keyword} setKeyword={setKeyword} />
+
+          {/* step 5 */}
+        </div>
       </div>
     </div>
   );
--- server/controllers/sub.js
+++ server/controllers/sub.js
@@ -3,8 +3,8 @@ const slugify = require("slugify");
 
 exports.create = async (req, res) => {
   try {
-    const { name } = req.body;
-    const sub = await new Sub({ name, slug: slugify(name) }).save();
+    const { name, parent } = req.body;
+    const sub = await new Sub({ name, parent, slug: slugify(name) }).save();
     res.json(sub);
   } catch (err) {
     res.status(400).send("Create Sub failed");

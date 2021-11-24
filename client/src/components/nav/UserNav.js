b607dd88e6284e84c20bf009cb97bf0813e6e3dd
TylerDurden75 <raytibo.j@gmail.com>
2021-11-24 16:36:52 +0100

    User nav clear

--- client/src/components/nav/UserNav.js
+++ client/src/components/nav/UserNav.js
@@ -7,7 +7,7 @@ const UserNav = () => {
       <nav>
         <ul className="nav flex-column">
           <li className="nav-item">
-            <Link to="/user/dashboard" className="nav-link">
+            <Link to="/user/history" className="nav-link">
               History
             </Link>
           </li>
--- client/src/components/routes/UserRoute.js
+++ client/src/components/routes/UserRoute.js
@@ -3,19 +3,17 @@ import { Routes } from "react-router";
 import { useSelector } from "react-redux";
 import { Route } from "react-router-dom";
 import LoadingToRedirect from "./LoadingToRedirect";
-import UserNav from "../nav/UserNav";
 
 import History from "../../pages/user/History";
 import Password from "../../pages/user/Password";
 import Wishlist from "../../pages/user/Wishlist";
 
 const UserRoute = ({ children, ...rest }) => {
-  // let location = useLocation();
   const { user } = useSelector((state) => ({ ...state }));
 
   return user && user.token ? (
     <Routes>
-      <Route path="dashboard" element={<History />} />
+      <Route path="history" element={<History />} />
       <Route path="password" element={<Password />} />
       <Route path="wishlist" element={<Wishlist />} />
     </Routes>
@@ -24,9 +22,3 @@ const UserRoute = ({ children, ...rest }) => {
   );
 };
 export default UserRoute;
-
-// if (!user) {
-//   return <Navigate to="/register" state={{ from: location }} {...rest} />;
-// }
-
-// return children;

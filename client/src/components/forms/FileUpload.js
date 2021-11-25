dcff4b0bbb19af296bb684a56d2044fd30722545
TylerDurden75 <raytibo.j@gmail.com>
2021-11-25 13:06:53 +0100

    Resizer files in client

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,31 @@
 import React from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import Resizer from "react-image-file-resizer";
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   const fileUploadAndResize = (e) => {
     // console.log(e.target.files);
+    //resize
+    let files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          "base64"
+        );
+      }
+    }
   };
 
   return (

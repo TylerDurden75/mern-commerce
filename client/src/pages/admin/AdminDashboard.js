73516313aca4215f6e6c4773a19f16856302ded2
TylerDurden75 <raytibo.j@gmail.com>
2021-11-25 15:34:44 +0100

    Fetching Product in AdminDashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AdminProductCard from "../../components/cards/AdminProductCard";
 import AdminNav from "../../components/nav/AdminNav";
+import { getProductsByCount } from "../../functions/product";
 
 const AdminDashboard = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2 border-right">
           <AdminNav />
         </div>
-        <div className="col">Admin dashboard page</div>
+
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <AdminProductCard key={product._id} product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

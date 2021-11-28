9505e95f92e17f1103174333a1b476bcf91cb4ba
TylerDurden75 <raytibo.j@gmail.com>
2021-11-28 17:00:36 +0100

    Fetching products in Home Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    showAllProducts();
+  }, []);
+
+  const showAllProducts = () => {
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+    });
+  };
+
   return (
     <div>
       <p>Home</p>
+      {JSON.stringify(products)}
     </div>
   );
 };

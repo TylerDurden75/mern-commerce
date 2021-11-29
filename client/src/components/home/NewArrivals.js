4b04bde574e89e1e73cab8257849c683ca11552e
TylerDurden75 <raytibo.j@gmail.com>
2021-11-29 12:42:29 +0100

    New Arrivals component

---
+++ client/src/components/home/NewArrivals.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../../functions/product";
+import ProductCard from "../cards/ProductCard";
+import LoadingCard from "../cards/LoadingCard";
+
+const NewArrivals = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    showAllProducts();
+  }, []);
+
+  const showAllProducts = () => {
+    setLoading(true);
+    getProducts("createdAt", "desc", 3).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <div className="container">
+        {loading ? (
+          <LoadingCard count={3} />
+        ) : (
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NewArrivals;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { getProductsByCount } from "../functions/product";
-import ProductCard from "../components/cards/ProductCard";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import LoadingCard from "../components/cards/LoadingCard";
+import NewArrivals from "../components/home/NewArrivals";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    showAllProducts();
-  }, []);
-
-  const showAllProducts = () => {
-    setLoading(true);
-    getProductsByCount(3).then((res) => {
-      setProducts(res.data);
-      setLoading(false);
-    });
-  };
-
   return (
     <>
       <div className="jumbotron jumbotron-fluid bg-dark h1 font-weight-bold text-white text-center">
@@ -28,19 +11,13 @@ const Home = () => {
         />
       </div>
 
-      <div className="container">
-        {loading ? (
-          <LoadingCard count={3} />
-        ) : (
-          <div className="row">
-            {products.map((product) => (
-              <div key={product._id} className="col-md-4">
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      <h4 className="jumbotron text-center p-3 mt-5 mb-5 display-4">
+        New Arrivals
+      </h4>
+      <NewArrivals />
+
+      <br />
+      <br />
     </>
   );
 };

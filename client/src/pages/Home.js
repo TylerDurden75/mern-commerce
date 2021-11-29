abd5c3b936dc0d85c38e1de1f75a3fc59e50c7ea
TylerDurden75 <raytibo.j@gmail.com>
2021-11-29 12:51:27 +0100

    Best Sellers component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
   return (
@@ -15,6 +16,10 @@ const Home = () => {
         New Arrivals
       </h4>
       <NewArrivals />
+      <h4 className="jumbotron text-center p-3 mt-5 mb-5 display-4">
+        Best Sellers
+      </h4>
+      <BestSellers />
 
       <br />
       <br />

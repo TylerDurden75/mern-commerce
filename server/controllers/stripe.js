ac50e17ba8703409b7e3042c2d86f190348226c9
TylerDurden75 <raytibo.j@gmail.com>
2021-12-03 21:36:55 +0100

    Charging actual cart total

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,9 +5,12 @@ const Coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  const { cartTotal } = await Cart.findOne({ orderedBy: user._id }).exec();
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1000,
-    payment_method_types: ["card"],
+    amount: cartTotal * 100,
     currency: "usd",
   });
 

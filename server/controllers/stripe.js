489dfd4240a3b6c6a5a4b04d550158f319140273
TylerDurden75 <raytibo.j@gmail.com>
2021-12-03 20:10:51 +0100

    Stripe payment setup

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -4,4 +4,14 @@ const Product = require("../models/product");
 const Coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 
-exports.createPaymentIntent = async (req, res) => {};
+exports.createPaymentIntent = async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    payment_method_types: ["card"],
+    currency: "usd",
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};

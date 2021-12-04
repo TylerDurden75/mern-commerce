804742651349228359dcb16539da676dc5a04fbf
TylerDurden75 <raytibo.j@gmail.com>
2021-12-04 22:33:56 +0100

    Purchase order in card

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -7,7 +7,7 @@ import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { toast } from "react-toastify";
 
 const History = () => {
-  const [orders, serOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
   const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
@@ -17,8 +17,23 @@ const History = () => {
   const showUserOrders = () =>
     getUserOrders(user.token).then((res) => {
       console.log(JSON.stringify(res.data, null, 4));
-      // setOrders(res.data);
+      setOrders(res.data);
     });
+
+  const showOrderInTable = (order) => <p>each order product</p>;
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <p>show payment info</p>
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF DOWLOAD</p>
+          </div>
+        </div>
+      </div>
+    ));
 
   return (
     <div className="container-fluid">
@@ -26,7 +41,10 @@ const History = () => {
         <div className="col-md-2 border-right">
           <UserNav />
         </div>
-        <div className="col">user history page</div>
+        <div className="col pt-2 text-center">
+          <h4>{orders.length > 0 ? "User purchase orders" : "No purchase"}</h4>
+          {showEachOrders()}
+        </div>
       </div>
     </div>
   );

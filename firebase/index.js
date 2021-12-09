7b79abc9ee3767858ecad2bf417a76b6240731b8
TylerDurden75 <raytibo.j@gmail.com>
2021-12-09 14:04:12 +0100

    Final deployment

--- firebase/index.js
+++ firebase/index.js
@@ -9,7 +9,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, " "),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   }),
 });
 

70c2bdee759b3708a74deeecab4f6ed8a069372b
TylerDurden75 <raytibo.j@gmail.com>
2021-12-09 13:52:51 +0100

    Final deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 //app
@@ -27,6 +28,18 @@ app.use(cors());
 
 //routes middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api Running");
+  });
+}
 
 //port
 const port = process.env.PORT || 8000;

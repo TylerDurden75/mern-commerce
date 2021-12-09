948a5b95dc09dd40c4851dc901f90dd4a22994ea
TylerDurden75 <raytibo.j@gmail.com>
2021-12-09 20:17:58 +0100

    fixing proxy

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors());
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
